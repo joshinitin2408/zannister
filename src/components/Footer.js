ad8bf6a31c468f531926bbd8972f35621fc24e9b
Nitin Joshi <joshinitin2408@gmail.com>
2025-04-14 22:43:05 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="d-flex flex-wrap justify-content-between align-items-center border-top bg-secondary text-light px-2">
-            <p className="col-md-4 mb-0">© 2018 - Zannister Software Solutions. All Rights Reserved</p>
+            <p className="col-md-4 mb-0">© 2025 - Zannister Software Solutions. All Rights Reserved</p>
 
             <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
                 <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
